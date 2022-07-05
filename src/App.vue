f0eae296ecd31b4fbbfaf2ec983dd5c4d278ae9e
shoaibuddin12fx <shoaibuddin12fx@gmail.com>
2022-07-05 21:01:05 +0500

    updated

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<!-- <template>
+  <div>
+    <h1>data binding in vuejs</h1>
+    <databind/>
+  </div>
+</template>
+<script>
+import databind from './components/databind.vue'
+export default{
+  components:{
+    databind,
+  }
+}
+</script>
+<style>
+
+</style>  -->
+
+
+<template>
+  <div>
+    <h1>event in vuejs</h1>
+    <databind/>
+  </div>
+</template>
+<script>
+import databind from './components/databind.vue'
+export default{
+  components:{
+    databind,
+  }
+}
+</script>
+
+
+
+
---
+++ src/components/databind.vue
@@ -0,0 +1,77 @@
+<!-- <template>
+    <div>
+        <h1>{{message}}</h1>
+        <textarea type="text" v-model="message" />
+
+    </div>
+</template>
+<script>
+
+
+export default {
+   // name:'homebind',
+    data(){
+        return{
+            message:'hi'
+        }
+    }
+        
+    
+}
+</script>
+<style>
+h1{
+    white-space: pre-line;
+}
+</style> -->
+
+
+
+
+<!-- <template>
+<div>
+<h2 v-for="(name,index) in names" :key="name">{{index}}{{name}}</h2> 
+<h3 v-for="(name,i) in fullNames" :key="name.first">{{i}} {{name.first}} {{name.last}}</h3> 
+
+
+</div>
+</template>
+<script>
+export default{
+  name:'databind',
+  data(){
+    return{
+      names:['umair','ahmed','saad'],
+      fullNames:[
+            {first:'umair', last:'qureshi'},
+            {first:'ahmed', last:'saim'},
+            {first:'saad', last:'rehman'}
+      ],
+    }
+  },
+}
+</script> -->
+
+<template>
+<div>
+    <button v-on:click="hello('data')">call hello function</button>
+</div>
+
+</template>
+<script>
+export default {
+    name:'databind',
+    props:{
+        data:String,
+        msg:String
+        
+    },methods: {
+        hello(item){
+            console.log(item);
+        }
+    }
+ 
+}
+</script>
+
+
